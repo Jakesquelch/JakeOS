c57a98cc8c573174065fff1e8e26da4eac598a00
Jake Squelch <jakewsquelch@gmail.com>
2025-11-29 14:32:01 +0000

    Added routing to my components and also created a landing page component too

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
+import { ToDoList } from './to-do-list/to-do-list';
+import { Journal } from './journal/journal';
+import { HabitTracker } from './habit-tracker/habit-tracker';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPage },
+  { path: 'todo', component: ToDoList },
+  { path: 'journal', component: Journal },
+  { path: 'habit', component: HabitTracker },
+];
